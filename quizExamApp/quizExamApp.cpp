2cd487335895f209127fab541cae8891404646ca
uiunter <tomovdmitrii@gmail.com>
2020-12-11 12:08:57 +0300

    added fstream from txt file

--- quizExamApp/quizExamApp.cpp
+++ quizExamApp/quizExamApp.cpp
@@ -2,6 +2,9 @@
 #include <conio.h>
 #include <windows.h>
 #include <cstdlib>
+#include <fstream>
+#include <string>
+#include <vector>
 
 
 HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE); // used for goto
@@ -14,11 +17,90 @@ void gotoXY(int x, int y)
 	CursorPosition.X = x;
 	CursorPosition.Y = y;
 	SetConsoleCursorPosition(console, CursorPosition);
-}// function defined below if this is new to you.
+}
 
 void cls()
 {
 	system("CLS");
+}
+
+void readFile(const char *filename)
+{
+	std::string line;
+	std::vector<std::string> lines;
+
+
+	//input file stream
+	std::ifstream file(filename);
+
+	//count number of total lines in the file and store the lines in the string vector
+	int total_lines = 0;
+	while (getline(file, line))
+	{
+		total_lines++;
+		lines.push_back(line);
+	}
+
+	//generate a random number between 0 and count of total lines
+	int random_number = rand() % total_lines;
+
+
+
+	//fetch the line where line index (starting from 0) matches with the random number
+	gotoXY(18, 5);
+	std::cout << lines[random_number];
+
+	int rightAnswer = 0, item = 0, x = 9;
+	bool running = true;
+	while (running)
+	{
+		gotoXY(20, 9);
+		std::cout << "Yes";
+		gotoXY(20, 10);
+		std::cout << "Next question";
+
+		system("pause>nul");
+
+		if (GetAsyncKeyState(VK_DOWN) && x != 10) //down button pressed
+		{
+			gotoXY(18, x);
+			std::cout << "  ";
+			x++;
+			gotoXY(18, x);
+			std::cout << "> ";
+			item++;
+			continue;
+		}
+
+		if (GetAsyncKeyState(VK_UP) && x != 9) //up button pressed
+		{
+			gotoXY(18, x);
+			std::cout << "  ";
+			x--;
+			gotoXY(18, x);
+			std::cout << "> ";
+			item--;
+			continue;
+		}
+
+		if (GetAsyncKeyState(VK_RETURN)) { // Enter key pressed
+
+			switch (item) {
+
+			case 0: {
+				++rightAnswer;
+				break;
+			}
+
+
+			case 1: {
+				cls();
+				readFile(filename);
+				break;
+			}
+			}
+		}
+	}
 }
 
 void subMenu()
@@ -31,13 +113,13 @@ void subMenu()
 	while (running_subMenu)
 	{
 		gotoXY(20, 7);
-		std::cout << "1) Matan";
+		std::cout << "1. Matan";
 		gotoXY(20, 8);
-		std::cout << "2) AIG";
+		std::cout << "2. AIG";
 		gotoXY(20, 9);
-		std::cout << "3) Proga";
+		std::cout << "3. Proga";
 		gotoXY(20, 10);
-		std::cout << "4) Return";
+		std::cout << "4. Return";
 
 		system("pause>nul");
 
@@ -70,10 +152,10 @@ void subMenu()
 				case 0: {
 
 					gotoXY(20, 16);
-					std::cout << "You chose Matan    ";
+					cls();
+					readFile("matan.txt");
 					break;
 				}
-
 
 				case 1: {
 					gotoXY(20, 16);
@@ -110,7 +192,7 @@ void Menu()
 	while (running)
 	{
 		gotoXY(20, 7);
-		std::cout << "1) Input";
+		std::cout << "1. Input";
 		gotoXY(20, 8);
 		std::cout << "Quit Program";
 
@@ -156,13 +238,10 @@ void Menu()
 					gotoXY(20, 16);
 					std::cout << "The program has now terminated!!";
 					exit(0);
-					running = false;
-					break;
+				
 				}
 			}
-
 		}
-
 	}
 
 	gotoXY(20, 21);
@@ -170,6 +249,7 @@ void Menu()
 
 int main()
 {
+	setlocale(LC_ALL, "Russian");
 	Menu();
 	return 0;
 }
