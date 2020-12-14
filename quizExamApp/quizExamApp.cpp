d19401ffbe79391e8115b88e53b976316d37f07b
uiunter <tomovdmitrii@gmail.com>
2020-12-14 22:01:22 +0300

    c

--- quizExamApp/quizExamApp.cpp
+++ quizExamApp/quizExamApp.cpp
@@ -3,8 +3,10 @@
 #include <windows.h>
 #include <cstdlib>
 #include <fstream>
+#include <functional>
 #include <string>
 #include <vector>
+#include <stdio.h>
 
 
 HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE); // used for goto
@@ -24,44 +26,61 @@ void cls()
 	system("CLS");
 }
 
-void readFile(const char *filename)
+void subMenu();
+bool iterateFile(const std::string& fileName, std::function<void(const std::string&)> callback)
 {
-	std::string line;
-	std::vector<std::string> lines;
-
-
-	//input file stream
-	std::ifstream file(filename);
-
-	//count number of total lines in the file and store the lines in the string vector
-	int total_lines = 0;
-	while (getline(file, line))
+	// Open the File
+	std::ifstream in(fileName.c_str());
+	// Check if object is valid
+	if (!in)
 	{
-		total_lines++;
-		lines.push_back(line);
+		std::cerr << "Cannot open the File : " << fileName << std::endl;
+		return false;
 	}
+	std::string str;
+	// Read the next line from File untill it reaches the end.
+	while (std::getline(in, str))
+	{
+		// Call the given callback
+		callback(str);
+	}
+	//Close The File
+	in.close();
+	return true;
+}
 
-	//generate a random number between 0 and count of total lines
-	int random_number = rand() % total_lines;
+void readFile(std::string fileName)
+{
+	//std::string line;
+	int totalLines = 0;
+	std::vector<std::string> lines;
+	bool res = iterateFile(fileName, [&](const std::string& str) {
+		// Add to vector
+		lines.push_back(str);
+		totalLines++;
+		});
 
+	int random_number = rand() % totalLines;
 
-
-	//fetch the line where line index (starting from 0) matches with the random number
 	gotoXY(18, 5);
 	std::cout << lines[random_number];
 
-	int rightAnswer = 0, item = 0, x = 9;
-	bool running = true;
+	int item = 0, x = 9;
+	bool running = true, run = true;
 	while (running)
 	{
 		gotoXY(20, 9);
 		std::cout << "Yes";
 		gotoXY(20, 10);
-		std::cout << "Next question";
+		//std::cout << "Next question";
+		//gotoXY(20, 11);
+		std::cout << "Return";
+		//gotoXY(18, 18);
+		//std::cout << "Right Answers: " << rightAnswers;
 
 		system("pause>nul");
 
-		if (GetAsyncKeyState(VK_DOWN) && x != 10) //down button pressed
+		if (GetAsyncKeyState(VK_DOWN) && x != 11) //down button pressed
 		{
 			gotoXY(18, x);
 			std::cout << "  ";
@@ -87,17 +106,23 @@ void readFile(const char *filename)
 
 			switch (item) {
 
-			case 0: {
-				++rightAnswer;
-				break;
-			}
+				case 0: {
+					cls();
+					readFile(fileName);
+					break;
+				}
 
-
-			case 1: {
-				cls();
-				readFile(filename);
-				break;
-			}
+				/*case 1: {
+					cls();
+					readFile(fileName);
+					break;
+				}
+				*/
+				case 2: {
+					cls();
+					subMenu();
+					break;
+					}
 			}
 		}
 	}
@@ -109,17 +134,17 @@ void subMenu()
 	gotoXY(18, 5);
 	std::cout << "Submenu";
 	bool running_subMenu = true;
-	int subMenu_item = 0, run, x = 7;
+	int subMenu_item = 0, x = 7;
 	while (running_subMenu)
 	{
 		gotoXY(20, 7);
-		std::cout << "1. Matan";
+		std::cout << "1. Математический Анализ";
 		gotoXY(20, 8);
-		std::cout << "2. AIG";
+		std::cout << "2. Алгебра и Геометрия";
 		gotoXY(20, 9);
-		std::cout << "3. Proga";
+		std::cout << "3. Программирование";
 		gotoXY(20, 10);
-		std::cout << "4. Return";
+		std::cout << "4. Назад";
 
 		system("pause>nul");
 
@@ -159,13 +184,15 @@ void subMenu()
 
 				case 1: {
 					gotoXY(20, 16);
-					std::cout << "You chose AIG     ";
+					cls();
+					readFile("aig.txt");
 					break;
 				}
 
 				case 2: {
 					gotoXY(20, 16);
-					std::cout << "You chose Proga    ";
+					cls();
+					readFile("prog.txt");
 					break;
 				}
 
@@ -181,20 +208,20 @@ void subMenu()
 
 void Menu()
 {
-	int menu_item = 0, run, x = 7;
+	int menu_item = 0, x = 7;
 	bool running = true;
 
 	gotoXY(18, 5);
-	std::cout << "Main Menu";
+	std::cout << "Главное меню";
 	gotoXY(18, 7);
 	std::cout << "> ";
 
 	while (running)
 	{
 		gotoXY(20, 7);
-		std::cout << "1. Input";
+		std::cout << "1. Начать";
 		gotoXY(20, 8);
-		std::cout << "Quit Program";
+		std::cout << "   Выйти";
 
 		system("pause>nul"); // the >nul bit causes it the print no message
 
@@ -236,7 +263,7 @@ void Menu()
 
 				case 1: {
 					gotoXY(20, 16);
-					std::cout << "The program has now terminated!!";
+					std::cout << "Программа завершила свое действие.";
 					exit(0);
 				
 				}
